438a46c39292b76c43c532a7349a853946a89b5e
CovChEGG <covchegg@gmail.com>
2022-02-19 18:58:00 +0700

    metod to print array of strings added

--- FinalWork/Program.cs
+++ FinalWork/Program.cs
@@ -6,3 +6,17 @@
 // ["hello", "2", "world", ":-)"] -> ["2", ":-)"]
 // ["1234", "1567", "-2", "computer science"] -> ["-2"]
 // ["Russia", "Denmark", "Kazan"] -> []
+
+void PrintArrayOfStrings(string[] arrayOfStrings)
+{
+    Console.Write("[");
+    for (int i = 0; i < arrayOfStrings.Length; i++)
+    {
+        Console.Write($"\"{arrayOfStrings[i]}\"");
+        if (i != arrayOfStrings.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+}
