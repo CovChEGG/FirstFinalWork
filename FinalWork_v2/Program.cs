fa61e5d0049826cc72eebf8dece598f7c4baa847
CovChEGG <covchegg@gmail.com>
2022-02-24 01:58:40 +0700

    create new methods to use V2 code without Split

--- FinalWork_v2/Program.cs
+++ FinalWork_v2/Program.cs
@@ -1,9 +1,76 @@
-﻿string[] InputStringFromConsole(char[] separators)
+﻿// we can reduce the code using the built-in features
+/*
+string[] InputStringFromConsole(char[] separators)
 {
     string inputStr = Console.ReadLine() ?? String.Empty;
     string[] stringArray = inputStr.Split( separators , StringSplitOptions.RemoveEmptyEntries);
     Console.Clear();
     return stringArray;
+}
+*/
+
+bool IsSeparatorCheck(char element, char[] separatorsArray)
+{
+    foreach (var item in separatorsArray)
+    {
+        if (item == element) return true;
+    }
+    return false;
+}
+
+int HowManyWords(string inputStr, char[] separatorsArray)
+{
+    int numOfWords = 0;
+    int isWord = 0;
+    foreach (var element in inputStr)
+    {
+        if (IsSeparatorCheck(element, separatorsArray))
+        {
+            isWord = 0;
+        }
+        else
+        {
+            if (isWord == 0)
+            {
+                numOfWords++;
+                isWord = 1;
+            }
+        }
+    }
+    return numOfWords;
+}
+
+string[] InputStringFromConsole(char[] separatorsArray)
+{
+    string inputStr = Console.ReadLine() ?? String.Empty;
+    int numOfWords = HowManyWords(inputStr, separatorsArray);
+    string[] inputStringsArray = new string[numOfWords];
+    int indexStrArr = 0;
+    int isWord = 0;
+    foreach(var currentElement in inputStr)
+    {
+        if (IsSeparatorCheck(currentElement, separatorsArray))
+        {
+            if (isWord == 1)
+            {
+                indexStrArr++;
+                isWord = 0;
+            }
+        }
+        else
+        {
+            if (isWord == 1)
+            {
+                inputStringsArray[indexStrArr] += Convert.ToString(currentElement);
+            }
+            else
+            {
+                inputStringsArray[indexStrArr] = Convert.ToString(currentElement);
+                isWord = 1;
+            }
+        }
+    }
+    return inputStringsArray;
 }
 
 void PrintStringsArray(string[] arrayOfStrings)
@@ -28,12 +95,12 @@ void PrintTwoArraysOfStrings(string[] initialStringsArray, string[] resultString
     Console.WriteLine();
 }
 
-int CalcStringsNumberWithSizeEqualOrLessNeeded(string[] stringsArray, int stringSize)
+int CalcStringsNumberWithSizeEqualOrLessNeeded(string[] arrayOfStrings, int sizeOfString)
 {
     int numberOfStrings = 0;
-    for (int i = 0; i < stringsArray.Length; i++)
+    foreach (var currentString in arrayOfStrings)
     {
-        if (stringsArray[i].Length <= stringSize)
+        if (currentString.Length <= sizeOfString)
         {
             numberOfStrings++;
         }
@@ -59,11 +126,12 @@ string[] CopyToNewArrayStringsWithSizeEqualOrLessNeeded(string[] stringsArray, i
 
 Console.Clear();
 
-Console.WriteLine("Input words through spaces for analizing text:");
-char[] separators = {' '};
+Console.WriteLine("Input words through \"spaces\" for text analysis and press \"Enter\":");
+char[] separators = { ' ' }; //You may use one char symbols for separator
 string[] initialStringsArray = InputStringFromConsole(separators);
 int stringSizeParameter = 3;
 string[] resultStringsArray = CopyToNewArrayStringsWithSizeEqualOrLessNeeded(initialStringsArray, stringSizeParameter);
+Console.Clear();
 PrintTwoArraysOfStrings(initialStringsArray, resultStringsArray);
 
 Console.WriteLine();
